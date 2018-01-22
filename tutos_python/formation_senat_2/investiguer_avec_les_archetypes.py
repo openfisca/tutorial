b99261cd4cde11820358980a7fe44fa6e51f66aa
Anna-Livia <annalivia.gomart@gmail.com>
2018-01-22 19:01:03 +0100

    Ajoute l'export en csv

--- tutos_python/formation_senat_2/investiguer_avec_les_archetypes.py
+++ tutos_python/formation_senat_2/investiguer_avec_les_archetypes.py
@@ -4,34 +4,51 @@
 
 import openfisca_france
 from openfisca_core.simulations import Simulation
-from openfisca_core import reforms
-from openfisca_core import periods
 import json
+import csv
 
 
-####### Décrivez les entités ###########
+####### Listez les entités ###########
 
-with open('situations/celib_smic_aucune_aides.json') as json_data:
-    json1_str = json_data.read()
-    situation = json.loads(json1_str)
-    print(json_data.name)
+situations_json = [
+    'situations/celib_smic_aucune_aides.json',
+    'situations/couple_retraites.json'
+]
 
-####### Calculez la situation actuelle de la legislation française ##############
+####### Listez les calculs à effectuer et les periodes sur lequels les calculer ###########
 
-legislation_france = openfisca_france.FranceTaxBenefitSystem()
-simulation_actuelle = Simulation(tax_benefit_system=legislation_france, simulation_json=situation)
+calculs = {
+    'rfr': '2017',
+    'aah_base_ressources': '2018-01',
+    'ass_base_ressources': '2018-01',
+    'cmu_base_ressources': '2018-01',
+    'prestations_familiales_base_ressources': '2018-01'
+}
 
-# Insérez ci-dessous la variable que vous souhaitez calculer (ex : 'impots_directs')
-# ainsi que la periode sur laquelle vous souaitez la calculer (ex : '2017')
+##### Initialiser le fichier CSV pour export Excel ######
 
-print "Revenu Fiscal de Référence"
-print simulation_actuelle.calculate('rfr', '2017')
-print '---'
+with open('resultats.csv', 'w') as csvfile:
+    fieldnames = ['Situation'] + list(calculs)
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
 
-bases_ressources = ['aah_base_ressources', 'ass_base_ressources', 'cmu_base_ressources', 'prestations_familiales_base_ressources']
+    for situation_json in situations_json:
+        with open(situation_json) as json_data:
+            json_str = json_data.read()
+            situation = json.loads(json_str)
 
-for base in bases_ressources:
-    print base
-    print simulation_actuelle.calculate(base, '2018-01')
-    print '---'
+        results = {"Situation": situation_json}
 
+        ####### Initialisez la simulation (rencontre des entités avec la legislation) ##############
+
+        legislation_france = openfisca_france.FranceTaxBenefitSystem()
+        simulation_actuelle = Simulation(tax_benefit_system=legislation_france, simulation_json=situation)
+
+        ##### Demandez l'ensemble des calculs #####
+        for calcul, period in calculs.iteritems():
+            results[calcul] = simulation_actuelle.calculate(calcul, period)[0]
+
+        ##### Ecrire les resultats dans le CSV
+        writer.writerow(results)
+
+print 'le calcul est terminé'
