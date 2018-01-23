5d7181933b95de961d6e0059a6fdbbac1721dd73
sandcha <sandcha@users.noreply.github.com>
2018-01-23 20:08:55 +0100

    Clean france v19 to v20 migration script

--- python/scripts/france_situation_from_v19_to_v20.sh
+++ python/scripts/france_situation_from_v19_to_v20.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-# Given a json situation on input, updates some enums from openfisca-france v19 to v20.
+# Update an OpenFisca situation in JSON format:
+# updates some enumerations from openfisca-france v19 to v20.
 
-## INPUT
-JSON_SITUATION="$1"
+if [ -z "$1" ]
+then
+    echo "[USAGE] sh france_situation_from_v19_to_v20.sh file.json"
+    echo "        Where 'file.json' is an OpenFisca situation in JSON format."
+else
+    # INPUT
 
-## ACTIONS
+    JSON_SITUATION="$1"
 
-gsed -i 's/Gir 5/gir_5/g' $JSON_SITUATION
-gsed -i 's/Gir 6/gir_6/g' $JSON_SITUATION
+    # ACTIONS
 
-#"aah_non_calculable":{"2018-01": de "" à "calculable"}
-#"rsa_non_calculable":{"2018-01": de "" à "calculable"}
-#KO
-#SRC="\"2018-01\": \"\""
-#DEST="\"2018-01\": \"calculable\""
-#gsed -i 's/$SRC/$DEST/g' $JSON_SITUATION
+    gsed -i 's/Gir 5/gir_5/g' $JSON_SITUATION
+    gsed -i 's/Gir 6/gir_6/g' $JSON_SITUATION
 
-gsed -i 's/Célibataire/celibataire/g' $JSON_SITUATION
-gsed -i 's/Marié/marie/g' $JSON_SITUATION
+    gsed -i 's/Célibataire/celibataire/g' $JSON_SITUATION
+    gsed -i 's/Marié/marie/g' $JSON_SITUATION
 
-gsed -i 's/Retraité/retraite/g' $JSON_SITUATION
+    gsed -i 's/Retraité/retraite/g' $JSON_SITUATION
 
-gsed -i "s/Locataire ou sous-locataire d'un logement loué vide non-HLM/locataire_vide/g" $JSON_SITUATION
-gsed -i "s/Propriétaire (non accédant) du logement/proprietaire/g" $JSON_SITUATION
+    gsed -i "s/Locataire ou sous-locataire d'un logement loué vide non-HLM/locataire_vide/g" $JSON_SITUATION
+    gsed -i "s/Propriétaire (non accédant) du logement/proprietaire/g" $JSON_SITUATION
+fi
