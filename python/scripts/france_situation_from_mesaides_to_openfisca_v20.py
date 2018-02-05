eb7666ef2e8203edcfb032944ac72749f966be20
sandcha <sandcha@users.noreply.github.com>
2018-02-05 17:49:52 +0100

    Replace json file content with update result

--- python/scripts/france_situation_from_mesaides_to_openfisca_v20.py
+++ python/scripts/france_situation_from_mesaides_to_openfisca_v20.py
@@ -16,19 +16,20 @@ import dpath.util
 
 # Fonctions principales
 
-def open_json(json_file):
+def update_json(json_file):
   with open(json_file, 'r+') as f:
     json_content = json.load(f)
     update_actions(f, json_content)
-    print_json(json_content)
-    # update_file(f, data)
+    update_file(f, json_content)
 
 
 def update_actions(file_stream, json_content):
+  # Ex : Mise à jour de la valeur d'id et vérification de succès
   id_key = u'_id'
   json_content = update_value(json_content, id_key, u'toto')
   print_value(json_content, id_key)
 
+  # Ex : Suppression d'id et vérification de succès
   json_content = delete_key(file_stream, json_content, id_key)
   print_value(json_content, id_key)
 
@@ -38,12 +39,18 @@ def update_actions(file_stream, json_content):
 
 def print_value(json_content, key):
   try:
-    print dpath.util.get(json_content, key)
+    print key + ": " + dpath.util.get(json_content, key)
   except KeyError as e:
     print 'Clef ' + e.message + ' introuvable.'
 
+
 def print_json(json_content):
   print json.dumps(json_content, indent=4, sort_keys=True)
+
+
+def add_key(json_content, new_key, new_value):
+  dpath.util.new(json_content, new_key, new_value)
+  return json_content
 
 
 def update_value(json_content, key, new_value):
@@ -54,24 +61,24 @@ def update_value(json_content, key, new_value):
 # Path rather than key ? ex : 'a/b/e/f/g'
 def update_key(json_content, old_key, new_key, new_value):
   json_content = delete_key(json_content, old_key)
-
-
-def delete_key(file_stream, json_content, key):
-  json_content.pop(key, None)
-  file_stream.seek(0)
-  # file_stream.truncate()
+  json_content = add_key(json_content, new_key, new_value)
   return json_content
 
 
-def add_key(json_content, new_key, new_value):
-  dpath.util.new(json_content, new_key, new_value)
+def delete_key(file_stream, json_content, key):
+  try:
+    json_content.pop(key, None)
+  except KeyError as e:
+    print 'Clef ' + e.message + ' introuvable.'
   return json_content
       
 
 def update_file(file, content):
-  json.dump(content, file, indent=4, sort_keys=True)
+  file.seek(0)
+  file.truncate()
+  file.write(json.dumps(content, file, indent=4, sort_keys=True))
 
 
-# Exemple :
+# Exécution de la mise à jour
 
-open_json(sys.argv[1])
+update_json(sys.argv[1])
