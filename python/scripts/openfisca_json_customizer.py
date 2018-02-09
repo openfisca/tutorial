d9faee82f7c4d0fc7bdd7cef627bb9c493353196
sandcha <sandcha@users.noreply.github.com>
2018-02-09 23:57:52 +0100

    Add script to delete mes-aides specific keys from json content

--- python/scripts/openfisca_json_customizer.py
+++ python/scripts/openfisca_json_customizer.py
@@ -20,9 +20,17 @@ where file.json is an OpenFisca situation in JSON format.
 import json
 import sys
 import dpath.util
+from dpath.exceptions import PathNotFound 
+import logging
 
 
 # Main functions
+
+handler = logging.StreamHandler()
+log = logging.getLogger(__name__)
+log.addHandler(handler)
+log.setLevel(logging.DEBUG)
+
 
 def update_json(json_file):
   with open(json_file, 'r+') as f:
@@ -32,14 +40,38 @@ def update_json(json_file):
 
 
 def update_actions(json_content):
-  # E.g. : Mise à jour de la valeur d'id et vérification de succès
-  id_key = u'_id'
+  # E.g. : Updating '_id' key value and priting it
+  # id_key = u'_id'
   # json_content = update_value(json_content, id_key, u'toto')
   # print_value(json_content, id_key)
 
-  # E.g. : Suppression d'id et vérification de succès
+  # E.g. : Updating json content from mes-aides.gouv.fr application structure to openfisca
+  from_mesaides_to_openfisca(json_content)
+
+
+def from_mesaides_to_openfisca(json_content):
+  '''
+  Removing mes-aides.gouv.fr application keys from given json content.
+  '''
+  id_key = u'_id'
+
+  paris_keys = ['paris_complement_sante', 
+    'paris_energie_famille', 'paris_forfait_famille', 
+    'paris_logement', 'paris_logement_familles', 
+    'paris_logement_aspeh', 'paris_logement_plfm', 'paris_logement_psol',
+    'parisien']
+
+  transport_keys = ['brest_metropole_transport', 'rennes_metropole_transport']
+
   json_content = delete_key(json_content, id_key)
-  print_value(json_content, id_key)
+
+  for key in paris_keys:
+    json_content = delete_key(json_content, 'familles/_/' + key)
+  
+  for key in transport_keys:
+    print_value(json_content, 'individus/demandeur/' + key)
+    json_content = delete_key(json_content, 'individus/demandeur/' + key)
+    json_content = delete_key(json_content, 'individus/enfant_0/' + key)
 
 
 # Helpers
@@ -47,13 +79,13 @@ def update_actions(json_content):
 
 def print_value(json_content, key):
   try:
-    print key + ": " + dpath.util.get(json_content, key)
+    log.info(key + u": " + str(dpath.util.get(json_content, key)))
   except KeyError as e:
-    print 'Clef ' + e.message + ' introuvable.'
+    log.error(e.message + u' key non found.')
 
 
 def print_json(json_content):
-  print json.dumps(json_content, indent=4, sort_keys=True)
+  log.info(json.dumps(json_content, indent=4, sort_keys=True))
 
 
 def add_key(json_content, new_key, new_value):
@@ -74,9 +106,13 @@ def update_key(json_content, old_key, new_key, new_value):
 
 def delete_key(json_content, key):
   try:
-    json_content.pop(key, None)
+    dpath.util.delete(json_content, key)
+    log.debug(key + u' key deleted.')
+
   except KeyError as e:
-    print 'Clef ' + e.message + ' introuvable.'
+    log.error(e.message + u' key not found.')
+  except PathNotFound as e:
+    log.error(e.message + u' path not found.')
   return json_content
       
 
@@ -88,4 +124,11 @@ def update_file(file, content):
 
 # Runs the modifying script on the json file
 
-update_json(sys.argv[1])
+try:
+  update_json(sys.argv[1])
+except BaseException as e:
+  if len(sys.argv) == 1:
+    log.error(u'Missing a .json file to customize.')
+    log.warn(u'USAGE : python openfisca_json_customizer.py file.json')
+  else:
+    log.error(str(e.__class__.__name__)+ ': ' + e.message)
