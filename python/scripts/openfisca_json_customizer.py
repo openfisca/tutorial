a95618cf4a5e92f617c9d6a9d40325f7f1a59320
sandcha <sandcha@users.noreply.github.com>
2018-02-10 00:56:39 +0100

    Add script to delete mes-aides pre-computed variables from json

--- python/scripts/openfisca_json_customizer.py
+++ python/scripts/openfisca_json_customizer.py
@@ -53,25 +53,45 @@ def from_mesaides_to_openfisca(json_content):
   '''
   Removing mes-aides.gouv.fr application keys from given json content.
   '''
-  id_key = u'_id'
+
+  json_content = delete_key(json_content, u'_id')
 
   paris_keys = ['paris_complement_sante', 
     'paris_energie_famille', 'paris_forfait_famille', 
     'paris_logement', 'paris_logement_familles', 
     'paris_logement_aspeh', 'paris_logement_plfm', 'paris_logement_psol',
     'parisien']
-
-  transport_keys = ['brest_metropole_transport', 'rennes_metropole_transport']
-
-  json_content = delete_key(json_content, id_key)
-
   for key in paris_keys:
     json_content = delete_key(json_content, 'familles/_/' + key)
   
+  transport_keys = ['brest_metropole_transport', 'rennes_metropole_transport']
   for key in transport_keys:
-    print_value(json_content, 'individus/demandeur/' + key)
     json_content = delete_key(json_content, 'individus/demandeur/' + key)
     json_content = delete_key(json_content, 'individus/enfant_0/' + key)
+
+  json_content = delete_mesaides_pre_computed_keys(json_content, '2018-01')
+
+
+def delete_mesaides_pre_computed_keys(json_content, period_str):
+  json_content = delete_key(json_content, 'familles/_/acs/' + period_str)
+  json_content = delete_key(json_content, 'familles/_/af/' + period_str)
+  json_content = delete_key(json_content, 'familles/_/aide_logement/' + period_str)
+  json_content = delete_key(json_content, 'familles/_/aide_logement_non_calculable/' + period_str)
+  json_content = delete_key(json_content, 'familles/_/asf/' + period_str)
+  json_content = delete_key(json_content, 'familles/_/asi/' + period_str)
+  json_content = delete_key(json_content, 'familles/_/aspa/' + period_str)
+  json_content = delete_key(json_content, 'familles/_/ass/' + period_str)
+  json_content = delete_key(json_content, 'familles/_/bourse_college/' + period_str)
+  json_content = delete_key(json_content, 'familles/_/bourse_lycee/' + period_str)
+  json_content = delete_key(json_content, 'familles/_/cf/' + period_str)
+  json_content = delete_key(json_content, 'familles/_/cmu_c/' + period_str)
+  json_content = delete_key(json_content, 'familles/_/paje_base/' + period_str)
+  json_content = delete_key(json_content, 'familles/_/ppa/' + period_str)
+  json_content = delete_key(json_content, 'familles/_/rsa/' + period_str)
+  json_content = delete_key(json_content, 'familles/_/rsa_non_calculable/' + period_str)
+  json_content = delete_key(json_content, 'individus/demandeur/aah/' + period_str)
+  json_content = delete_key(json_content, 'individus/demandeur/aah_non_calculable/' + period_str)
+  json_content = delete_key(json_content, 'individus/demandeur/acs/' + period_str)  # TODO Check existence.
 
 
 # Helpers
@@ -110,9 +130,9 @@ def delete_key(json_content, key):
     log.debug(key + u' key deleted.')
 
   except KeyError as e:
-    log.error(e.message + u' key not found.')
+    log.error(e.message)
   except PathNotFound as e:
-    log.error(e.message + u' path not found.')
+    log.error(e.message)
   return json_content
       
 
@@ -131,4 +151,7 @@ except BaseException as e:
     log.error(u'Missing a .json file to customize.')
     log.warn(u'USAGE : python openfisca_json_customizer.py file.json')
   else:
-    log.error(str(e.__class__.__name__)+ ': ' + e.message)
+    if e.message:
+      log.error(str(e.__class__.__name__)+ ': ' + e.message)
+    else:
+      log.error(e)
