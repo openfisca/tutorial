7ca9b79e39bcb33f86e0d8e80563092f7e2e9cbe
Thomas Guillet <guillet.thomas@gmail.com>
2018-02-22 14:40:05 +0100

    Modification reforms

--- python/exemples/gt_non_recours/framework.py
+++ python/exemples/gt_non_recours/framework.py
@@ -31,11 +31,13 @@ class aide_logement_base_ressources_initiale(Variable):
     definition_period = MONTH
 
     def formula(famille, period, parameters):
-        periods = [period] # monthly periods
-        salaire_imposable_individus = famille.members('salaire_imposable', periods, options = [ADD])
+        size_in_months = 1
+        ressource_period = period.start.period('month', size_in_months).offset(-size_in_months)
+
+        salaire_imposable_individus = famille.members('salaire_net', ressource_period, options = [ADD])
         salaire_imposable = famille.sum(salaire_imposable_individus)
 
-        annual_scaling = 12 / len(periods)
+        annual_scaling = 12 / size_in_months
         return salaire_imposable * 0.9 * annual_scaling
 
 
@@ -44,10 +46,10 @@ class AL_MM1(reforms.Reform):
     def apply(self):
         self.update_variable(aide_logement_base_ressources_initiale)
 
-al_reforms = [{'name':'Base', 'legislation': 'l2'}, { 'name': 'AL_MM1', 'legislation': 'l1' }]
-
-al_reforms = [{'name':'Base', 'legislation': legislation_france}, { 'name': 'AL_MM1', 'legislation': AL_MM1(legislation_france) }]
-
+al_reforms = [
+    { 'name': 'Base', 'legislation': legislation_france},
+    { 'name': 'AL_MM1', 'legislation': AL_MM1(legislation_france)}
+]
 
 stable = {
     'familles': {
@@ -91,6 +93,55 @@ stable = {
         }
     }
 }
+
+rupture_2017 = {
+    'familles': {
+        'f1': {
+            'parents': ['Faustine']
+        }
+    },
+    'foyers_fiscaux': {
+        'ff1': {
+            'declarants': ['Faustine']
+        }
+    },
+    'individus': {
+        'Faustine': {
+            'salaire_imposable': {
+                '2015': 12 * 700,
+                '2016': 12 * 700,
+                '2017': 12 * 400,
+                '2018': 12 * 400,
+                '2019': 12 * 400
+            }
+        }
+    },
+    'menages': {
+        'm1': {
+            'personne_de_reference': ['Faustine'],
+            'loyer': {
+                '2015': 12 * 500,
+                '2016': 12 * 500,
+                '2017': 12 * 500,
+                '2018': 12 * 500,
+                '2019': 12 * 500
+            },
+            'statut_occupation_logement': {
+                '2015': 'locataire_vide',
+                '2016': 'locataire_vide',
+                '2017': 'locataire_vide',
+                '2018': 'locataire_vide',
+                '2019': 'locataire_vide'
+            }
+        }
+    }
+}
+
+situations = {
+    #'stable': stable,
+    'rupture_2017': rupture_2017
+}
+
 years = [str(y).zfill(2) for y in range(2015, 2020)]
 monthIds = [ str(m).zfill(2) for m in range(1,13) ]
 allMonths = [y + '-' + m for y in years for m in monthIds]
@@ -100,40 +151,39 @@ year17 = '2017'
 periods = months2018
 
 calculs = {
-    'prestations_familiales_base_ressources_individu': allMonths,
-    'aide_logement': periods,
-    'aide_logement_base_ressources': periods,
-    'rsa_base_ressources_statique': periods,
-    'traitements_salaires_pensions_rentes': years,
-    'aide_logement_base_ressources_defaut': allMonths,
-    'aide_logement_base_ressources_eval_forfaitaire': allMonths,
-    'revenu_assimile_salaire_apres_abattements': years,
-    'abattement_salaires_pensions': years
+    'aide_logement_base_ressources': allMonths,
+    'aide_logement': allMonths,
+    'salaire_net': allMonths,
+    'loyer': allMonths,
+    'proportion_ressource_logement': allMonths,
+    'taux_effort_logement': allMonths
 }
 
 fieldnames = ['Situation', 'Reform', 'Period', 'Variable', 'Value']
 
-with open('resultats.csv', 'w') as csvfile:
+with open('resultats-.csv', 'w') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-    results = {'Situation': 'stable'}
 
-    ####### Initialisez la simulation (rencontre des entités avec la legislation) ##############
+    for (name, situation) in situations.iteritems():
+        results = {'Situation': name}
 
-    for reform in al_reforms:
+        ####### Initialisez la simulation (rencontre des entités avec la legislation) ##############
 
-        results['Reform'] = reform['name']
-        simulation_actuelle = Simulation(tax_benefit_system=reform['legislation'], simulation_json=stable)
+        for reform in al_reforms:
 
-        ##### Demandez l'ensemble des calculs #####
-        for calcul, periods in calculs.iteritems():
-            results['Variable'] = calcul
-            for period in periods:
-                results['Period'] = period
-                data = simulation_actuelle.calculate(calcul, period)
-                pprint(data)
-                results['Value'] = data[0]
-                writer.writerow(results)
+            results['Reform'] = reform['name']
+            simulation_actuelle = Simulation(tax_benefit_system=reform['legislation'], simulation_json=situation)
+
+            ##### Demandez l'ensemble des calculs #####
+            for calcul, periods in calculs.iteritems():
+                results['Variable'] = calcul
+                for period in periods:
+                    results['Period'] = period
+                    data = simulation_actuelle.calculate(calcul, period)
+                    pprint(data)
+                    results['Value'] = data[0]
+                    writer.writerow(results)
 
 print 'le calcul est terminé'
 
