c64df773ef56daa1ad0cc1d1833f7545ffc4f726
sandcha <sandcha@users.noreply.github.com>
2018-02-26 17:21:05 +0100

    Draw line according to periods for one reform

--- python/exemples/gt_non_recours/display_graph.py
+++ python/exemples/gt_non_recours/display_graph.py
@@ -7,26 +7,38 @@ import matplotlib.pyplot as plt  # For graphics
 from openfisca_core.simulations import Simulation
 from framework import calculs, situations, al_reforms
 
-figure = plt.figure(figsize=(12, 8))
-for (name, situation) in situations.iteritems():
-        results = {'Situation': name}
-        line = {}
+def draw():
+    i=0
+    for (name, situation) in situations.iteritems():
+        figure = plt.figure(figsize=(12, 8))
+        x_axis = []
+        x_axis_labels = []
 
-        for reform in al_reforms:
-            results['Reform'] = reform['name']
-            simulation_actuelle = Simulation(tax_benefit_system=reform['legislation'], simulation_json=situation)
+        # for reform in al_reforms:
+        reform = al_reforms[0]
+        simulation_actuelle = Simulation(tax_benefit_system=reform['legislation'], simulation_json=situation)
+            
+        y_axis = []
+        for calcul, periods in calculs.iteritems():
+            for period in periods:
+                if period not in x_axis:
+                    x_axis_labels.append(period)
+                    x_axis.append(len(x_axis_labels))  # 1, 2, 3, ...
 
-            for calcul, periods in calculs.iteritems():
-                results['Variable'] = calcul
-                for period in periods:
-                    results['Period'] = period
-                    data = simulation_actuelle.calculate(calcul, period)
-                    results['Value'] = data[0]
-                    line[period] = data[0]
+                data = simulation_actuelle.calculate(calcul, period)
+                y_axis.append(data[0])
 
-                plt.plot(calculs, line, label=reform['name'] + '.' + calcul)    
+            print "y_axis"
+            print len(y_axis)
+            print y_axis
 
-plt.xlabel(u'test')
-plt.legend()
-# plt.show(block=True)
-figure.savefig('figure')
+            plt.xticks(x_axis, x_axis_labels)
+            plt.plot(x_axis, y_axis, label=reform['name'] + '.' + calcul)
+
+        plt.xlabel(u'periods')
+        plt.legend()
+        plt.show(block=True)
+        figure.savefig('figure_' + str(i))
+        i += 1
+
+draw()
