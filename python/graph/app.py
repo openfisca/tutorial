e67fbb6660f22726447068136709c61a6378c591
sandcha <sandcha@users.noreply.github.com>
2018-06-15 15:00:43 +0200

    Add plotly example for visualization

--- python/graph/app.py
+++ python/graph/app.py
@@ -1,17 +1,36 @@
 # -*- coding: utf-8 -*-
 import dash
-import dash_core_components as dcc
+
+# a component for every HTML tag
 import dash_html_components as html
+
+# higher-level components that are interactive and are generated 
+# with JavaScript, HTML, and CSS through the React.js library
+import dash_core_components as dcc
+
+# plots
+import pandas as pd
+import plotly.graph_objs as go
+
 
 app = dash.Dash()
 
+# data for plots
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
+    'gdp-life-exp-2007.csv')
+
+
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='#dataFin - Simule ton amendement'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        https://forum.datafin.fr/t/simule-ton-amendement/214
     '''),
 
+    html.H2('bar-chart-example'),
     dcc.Graph(
         id='example-graph',
         figure={
@@ -20,11 +39,40 @@ app.layout = html.Div(children=[
                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
             ],
             'layout': {
-                'title': 'Dash Data Visualization'
+                'title': 'Visualisation des données'
             }
+        }
+    ),
+
+    html.H2('life-exp-vs-gdp'),
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
         }
     )
 ])
 
 if __name__ == '__main__':
+    app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
     app.run_server(debug=True)
