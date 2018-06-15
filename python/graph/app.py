d4b20752b1f4c628c522e7d01e1db77212126e9e
sandcha <sandcha@users.noreply.github.com>
2018-06-15 23:22:49 +0200

    Add labels to visualization chart

--- python/graph/app.py
+++ python/graph/app.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import dash
+from dash.dependencies import Input, Output
 
 # a component for every HTML tag
 import dash_html_components as html
@@ -8,30 +9,54 @@ import dash_html_components as html
 # with JavaScript, HTML, and CSS through the React.js library
 import dash_core_components as dcc
 
-
 import reform_to_graph
 
 
 app = dash.Dash()
 
+# To serve offline, uncomment these lines and check for css dependency:
+# app.css.config.serve_locally = True
+# app.scripts.config.serve_locally = True
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+app.title = '#datFin #214'
+# <link rel="stylesheet" type="text/css" href="/css/main.css">
 app.layout = html.Div(children=[
     html.H2(children='#dataFin - Simule ton amendement'),
 
     html.A(href='https://forum.datafin.fr/t/simule-ton-amendement/214'),
 
-    html.H3("PPA hier et aujourd'hui"),
+    html.Div(children=[
+        html.H3("PPA hier et aujourd'hui"),
+
+        # slider jouant sur le taux de dégressivité 
+        # = 1 - parameters.prestations.minima_sociaux.ppa.pente
+        # faire varier le taux de dégressivité entre [0, 1]
+        html.P(children=[
+            "Taux de dégressivité : ",
+            html.Span(id="taux-degressivite-ppa-value")
+        ]),
+        dcc.Slider(
+            id="taux-degressivite-ppa",
+            min=0,
+            max=100,
+            step=10,
+            value=50,
+        ),
+    ], style={'width': '33%'}),
+
     dcc.Graph(
-        id='example',
+        id='area-chart',
         figure=reform_to_graph.generate_graph()
     )
 
-    # slider jouant sur le taux de dégressivité 
-    # = 1 - parameters.prestations.minima_sociaux.ppa.pente
-    # faire varier le taux de dégressivité entre [0, 1]
 
     # un menu permettant de sélectionner la situation et d'afficher le résultat sur la ppa
 ])
 
+@app.callback(Output('taux-degressivite-ppa-value', 'children'), [Input('taux-degressivite-ppa', 'value')])
+def display_taux_degressivite_ppa(taux_degressivite_ppa):
+    return taux_degressivite_ppa / 100. 
+
+
 if __name__ == '__main__':
-    app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
     app.run_server(debug=True)
--- python/graph/reform_to_graph.py
+++ python/graph/reform_to_graph.py
@@ -8,19 +8,29 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 
 from ppa_reform import data_frame 
+# print data_frame
 
-print data_frame
 
 def generate_graph():
     trace1 = go.Scatter(
+        name="Législation actuelle",
         x=data_frame['actuel_salaire_net'],
         y=data_frame['actuel_ppa'],
-        fill='ppa actuelle'
+        # fill='tonexty'
     )
     trace2 = go.Scatter(
+        name="Réforme",
         x=data_frame['actuel_salaire_net'],
         y=data_frame['reforme_ppa'],
-        fill='reforme de la ppa'
+        # fill='tozeroy'
     )
     data = [trace1, trace2]
-    return go.Figure(data=data)
+    
+    layout = go.Layout(
+        title="Prime d'activité par salaire net",
+        # margin=go.Margin(l=50, r=50, b=50, t=50),
+        xaxis={'title': "salaire net (€)"},
+        yaxis={'title': "prime d'activité (€)"}
+    )
+    
+    return go.Figure(data=data, layout=layout)
