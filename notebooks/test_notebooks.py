960a99135383646bafead73c2fb3b3238e1d6037
sandcha <sandcha@users.noreply.github.com>
2018-10-30 11:35:23 +0100

    Clean up test_notebooks script

--- notebooks/test_notebooks.py
+++ notebooks/test_notebooks.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 
-
-'''
-Checks notebook execution result.
-Equal to this command + error management:
-jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=60 --output executed_notebook.ipynb demo.ipynb
-
-For nbconvert information, see: http://nbconvert.readthedocs.io/en/latest/index.html
-For jupyter configuration information, run: jupyter --path
-'''
-
-# Dependencies: nbformat, nbconvert, jupyter-client, ipykernel
-import io
-import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
 import sys
 import os
+from io import open
+
+from nbformat import read, write
+from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
+# Other dependencies: jupyter-client, ipykernel
+
 import logging
 import traceback
+
+
+'''
+Checks notebooks by trying to convert each one into a notebook
+that includes its execution results.
+
+This script is similar to this command:
+jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=60
+                  --output executed_notebook.ipynotebook demo.ipynb
+'''
 
 
 log = logging.getLogger(__name__)
@@ -26,61 +28,71 @@ log.setLevel(logging.DEBUG)
 
 
 def is_notebook(file_path):
-  return file_path[-6:] == ".ipynb"
+    return file_path[-6:] == ".ipynb"
 
 
 def run(notebook_path):
-    directory = os.path.dirname(notebook_path)
-    run_path = directory
+    '''
+    Test given notebook execution.
+    If an error occurs, save execution result in a new notebook 
+    of same name with 'executed_' prefix.
+    '''
+    notebook_directory = os.path.dirname(notebook_path)
     notebook_filename = os.path.basename(notebook_path)
-    notebook_filename_out = os.path.join(directory, 'executed_' + notebook_filename)
+    notebook_filename_out = os.path.join(
+        notebook_directory,
+        'executed_' + notebook_filename
+        )
 
-    with io.open(notebook_path) as f:
-        nb = nbformat.read(f, as_version=4)
+    with open(notebook_path) as f:
+        notebook = read(f, as_version=4)
 
-    ep = ExecutePreprocessor(timeout=600, kernel_name='python')
-    error = False
     try:
-        out = ep.preprocess(nb, {'metadata': {'path': run_path}})
+        # Execute all the cells in the notebook
+        ep = ExecutePreprocessor(timeout=600, kernel_name='python')
+        executed_notebook = ep.preprocess(notebook,
+                {'metadata': {'path': notebook_directory}}
+                )
+
     except CellExecutionError:
-        error = True
-        out = None
+        executed_notebook = None
         msg = 'Error executing the notebook "%s".\n\n' % notebook_filename
-        msg += 'See notebook "%s" for the traceback.' % notebook_filename_out
+        msg += 'See notebook "%s" for traceback.' % notebook_filename_out
         log.error(msg)
         raise
+
     finally:
-        with io.open(notebook_filename_out, mode='wt') as f:  # io.open avoids UnicodeEncodeError
-            nbformat.write(nb, f)
+        with open(notebook_filename_out, mode='wt') as f:
+            write(notebook, f)
+        if executed_notebook is not None:
+            os.remove(notebook_filename_out)
 
-    if not error:
-        os.remove(notebook_filename_out)
 
-
+# Check script target (file or directory) and test all notebooks
 if __name__ == "__main__":
-  try:
-    target = sys.argv[1]
+    try:
+        target = sys.argv[1]
 
-    if os.path.isdir(target):
-      for file in os.listdir(target):
-        if is_notebook(file):
-          log.debug("> " + file)
-          run(os.path.join(target, file))
-    else:
-        if not is_notebook(target):
-            raise Exception(u"Expected an .ipynb file. Got: {}".format(target))
-        run(target)
+        if os.path.isdir(target):
+            for file in os.listdir(target):
+                if is_notebook(file):
+                    log.debug("> " + file)
+                    run(os.path.join(target, file))
+        else:
+            if not is_notebook(target):
+                raise Exception(u"Expected an .ipynb file. Got: {}".format(target))
+            run(target)
 
-  except BaseException as e:
-    if len(sys.argv) == 1:
-      log.error(u"Missing a directory or a notebook to test.")
-      log.warn(u"USAGE : python test_notebooks.py target")
-      log.warn(u"        where 'target' is a directory containing .ipynb file OR an .ipynb file to test.")
-    else:
-      log.debug(str(e.__class__.__name__)+ ': ')
-      if e.__class__.__name__ is "CellExecutionError" and e.from_cell_and_msg:
-        log.debug(e.from_cell_and_msg)
-      elif e.message:
-        log.debug(e.message)
-      
-      log.error(traceback.format_exc())
+    except BaseException as e:
+        if len(sys.argv) == 1:
+            log.error(u"Missing a notebook directory or a notebook to test.")
+            log.warn(u"USAGE: python test_notebooks.py target")
+            log.warn(u"where 'target' is a notebooks directory OR an .ipynb notebook file to test.")
+
+        else:
+            log.debug(str(e.__class__.__name__) + ': ')
+            if e.__class__ is CellExecutionError and e.from_cell_and_msg:
+                log.debug(e.from_cell_and_msg)
+            elif e.message:
+                log.debug(e.message)
+            log.error(traceback.format_exc())
