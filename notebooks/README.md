49f71f590d252e4042cf4f8cf9de6f632cd67579
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2018-10-31 13:47:16 +0100

    Add README.md and fix Makefile

---
+++ notebooks/README.md
@@ -0,0 +1,36 @@
+# Tutorial Notebooks
+
+## How to test all notebooks
+
+In your console, run:
+
+```
+make install
+make test
+```
+
+If all goes according to plan, you should see a nice `OK` without any ceremonial message 😃
+
+Otherwise, you'll receive really handy feedback telling you where it doesn't work, and why:
+
+```
+---------------------------------------------------------------------------
+<ipython-input-2-6c6f6a2075ab> in <module>()
+----> 1 simulation.calculate('af', '2015')
+
+/Users/hyperion/.pyenv/versions/tutoriel/lib/python2.7/site-packages/openfisca_core/simulations.pyc in calculate(self, variable_name, period, **parameters)
+    153             self.tracer.record_calculation_start(variable.name, period, **parameters)
+    154
+--> 155         self._check_period_consistency(period, variable)
+    156
+    157         extra_params = parameters.get('extra_params', ())
+
+/Users/hyperion/.pyenv/versions/tutoriel/lib/python2.7/site-packages/openfisca_core/simulations.pyc in _check_period_consistency(self, period, variable)
+    288                 variable.name,
+    289                 period
+--> 290                 ).encode('utf-8'))
+    291
+    292         if variable.definition_period == periods.YEAR and period.unit != periods.YEAR:
+
+ValueError: Unable to compute variable 'af' for period 2015: 'af' must be computed for a whole month. You can use the ADD option to sum 'af' over the requested period, or change the requested period to 'period.first_month'.
+```
--- notebooks/test_notebooks.py
+++ notebooks/test_notebooks.py
@@ -13,12 +13,13 @@ import traceback
 
 
 '''
-Checks notebooks by trying to convert each one into a notebook
-that includes its execution results.
+Checks notebooks by trying to convert each one of them into a notebook that
+includes the execution results.
 
 This script is similar to this command:
-jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=60
-                  --output executed_notebook.ipynotebook demo.ipynb
+
+    jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=60 \
+                      --output executed_notebook.ipynotebook demo.ipynb
 '''
 
 
@@ -33,9 +34,10 @@ def is_notebook(file_path):
 
 def run(notebook_path):
     '''
-    Test given notebook execution.
-    If an error occurs, save execution result in a new notebook 
-    of same name with 'executed_' prefix.
+    Execute a notebook.
+
+    If an error occurs, then save results in a new notebook
+    of the name, prefixed bt "executed_".
     '''
     notebook_directory = os.path.dirname(notebook_path)
     notebook_filename = os.path.basename(notebook_path)
@@ -45,24 +47,25 @@ def run(notebook_path):
         )
 
     with open(notebook_path) as f:
-        notebook = read(f, as_version=4)
+        notebook = read(f, as_version = 4)
 
     try:
         # Execute all the cells in the notebook
-        ep = ExecutePreprocessor(timeout=600, kernel_name='python')
-        executed_notebook = ep.preprocess(notebook,
-                {'metadata': {'path': notebook_directory}}
-                )
+        ep = ExecutePreprocessor(timeout = 600, kernel_name = "python")
+        executed_notebook = ep.preprocess(
+            notebook,
+            {"metadata": {"path": notebook_directory}}
+            )
 
     except CellExecutionError:
         executed_notebook = None
         msg = 'Error executing the notebook "%s".\n\n' % notebook_filename
-        msg += 'See notebook "%s" for traceback.' % notebook_filename_out
+        msg += 'See notebook "%s" for stack traceback.' % notebook_filename_out
         log.error(msg)
         raise
 
     finally:
-        with open(notebook_filename_out, mode='wt') as f:
+        with open(notebook_filename_out, mode = "wt") as f:
             write(notebook, f)
         if executed_notebook is not None:
             os.remove(notebook_filename_out)
@@ -85,12 +88,12 @@ if __name__ == "__main__":
 
     except BaseException as e:
         if len(sys.argv) == 1:
-            log.error(u"Missing a notebook directory or a notebook to test.")
+            log.error(u"Missing notebook or directory containing notebooks to test.")
             log.warn(u"USAGE: python test_notebooks.py target")
-            log.warn(u"where 'target' is a notebooks directory OR an .ipynb notebook file to test.")
+            log.warn(u"where 'target' is directory containing notebooks, or a .ipynb notebook file.")
 
         else:
-            log.debug(str(e.__class__.__name__) + ': ')
+            log.debug(str(e.__class__.__name__) + ": ")
             if e.__class__ is CellExecutionError and e.from_cell_and_msg:
                 log.debug(e.from_cell_and_msg)
             elif e.message:
